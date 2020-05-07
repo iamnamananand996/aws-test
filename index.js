9d7dd11b662514cda5e9c01ac0213e31f86ef6c1
iamnamananand996 <iamnamananand996@gmail.com>
2020-05-07 18:24:14 +0530

    first commit 2

--- index.js
+++ index.js
@@ -5,21 +5,19 @@ const port = 5000;
 var mysql = require("mysql");
 var con = mysql.createConnection({
 	host: "localhost",
-	user: "admin",
-	password: "luxembourg",
+	user: "root",
+	password: "password",
 	database: "test",
 });
 
-app.get("/", (req, res) => {
-	con.connect(function (err) {
-		if (err) {
-			console.log("Error connecting to Db");
-			console.log(err);
-			res.send({ msg: "database not connected" }).status(400);
-		}
-		res.send({ msg: "database connected" }).status(200);
-	});
+con.connect(function (err) {
+	if (err) {
+		console.log("Error connecting to Db");
+	}
+	console.log("Connect to database");
 });
+
+app.get("/", (req, res) => {});
 
 app.listen(port, () =>
 	console.log(`Example app listening at http://localhost:${port}`)
